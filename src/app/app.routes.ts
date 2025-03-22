bd5d79beca5624abd29e241f624e9947d2f0e011
Laura <laura@info.com>
2025-03-22 18:33:26 +0100

    Rutas creadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UserViewComponent } from './pages/dashboard/user-view/user-view.component';
+import { UserFormComponent } from './pages/dashboard/user-form/user-form.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+
+//defino als rutas
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent }, //lista de usuarios
+  {
+    path: "dashboard", component: DashboardComponent,  children: //dashboard tiene rutas hijas
+    [
+        { path: 'user/:id', component: UserViewComponent }, // vista del usuario
+        { path: 'newuser', component: UserFormComponent }, // crear un usuario
+        { path: 'updateuser/:id', component: UserFormComponent }, // editar un usuario
+    ]},
+    { path: '**', redirectTo: '/home' } // errores redirecciona a home
+  ];
+  
+  
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [RouterOutlet],//barra de navegación
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
