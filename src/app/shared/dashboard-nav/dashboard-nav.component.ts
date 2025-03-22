093db9cf88a8dcbef94d13b1dca627d088d85f1d
Laura <laura@info.com>
2025-03-22 17:53:16 +0100

    Componentes creados

---
+++ src/app/shared/dashboard-nav/dashboard-nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-nav',
+  imports: [],
+  templateUrl: './dashboard-nav.component.html',
+  styleUrl: './dashboard-nav.component.css'
+})
+export class DashboardNavComponent {
+
+}
